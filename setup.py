d6f7467c428ee813b2a9c703e055653fb946d249
Samuel Williams <samueltwilliams@gmail.com>
2013-03-10 14:08:24 +0000

    fixing setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1.0-dev',
     packages=['p3'],
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description='a minimal d3 for python',
     install_requires=[
-        'cssselect >= 0.7.1'
+        'cssselect >= 0.7.1',
         'lxml >= 3.1.0'
     ]
 )
