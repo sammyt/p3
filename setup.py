021decf90df92a2af639226758f3c253269e65a3
Samuel Williams <samueltwilliams@gmail.com>
2013-03-10 14:21:40 +0000

    updated setup.py for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='p3',
-    version='0.1.0-dev',
+    version='0.1.1-dev',
     packages=['p3'],
     license='MIT',
     long_description='a minimal d3 for python',
