98df9db379a71cf49607dc96cdb13d50ea1e1265
Samuel Williams <samueltwilliams@gmail.com>
2013-03-16 14:23:39 +0000

    cleaning up pypi config

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='p3',
     version='0.1.2',
+    author='Sam Williams',
+    author_email='samueltwilliams@gmail.com',
     packages=['p3'],
     license='MIT',
     description='a minimal d3 for python',
+    classifiers=[
+        'Programming Language :: Python :: 2'
+    ],
     install_requires=[
         'cssselect >= 0.7.1',
         'lxml >= 3.1.0'
     ],
     setup_requires=['nose'],
-    tests_require=['sure>=1.1.7']
+    tests_require=['sure==1.1.7', 'coverage'],
 )
