ef548f52051fc24ce1596e73ebc703ff50effcb1
Samuel Williams <samueltwilliams@gmail.com>
2013-03-16 14:09:18 +0000

    most package clean up

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 
 setup(
     name='p3',
-    version='0.1.1-dev',
+    version='0.1.2',
     packages=['p3'],
     license='MIT',
-    long_description='a minimal d3 for python',
+    description='a minimal d3 for python',
     install_requires=[
         'cssselect >= 0.7.1',
         'lxml >= 3.1.0'
-    ]
+    ],
+    setup_requires=['nose'],
+    tests_require=['sure>=1.1.7']
 )
