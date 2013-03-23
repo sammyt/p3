6effcd2d4bbde858c6b1237493dce95baf8d4830
Samuel Williams <samueltwilliams@gmail.com>
2013-03-23 18:07:10 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='p3',
-    version='0.2.0',
+    version='0.2.1',
     author='Sam Williams',
     author_email='samueltwilliams@gmail.com',
     packages=['p3'],
